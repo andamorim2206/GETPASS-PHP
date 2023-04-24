8be3e495fb955766103ef8228126c6eeec8c9ff2
André Amorim <deds5879@gmail.com>
2023-04-23 22:05:06 -0300

    routes

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function findAll(Request $user)
+    {
+        echo 'hello World';
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,7 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-Route::namespace('App\Http\Controllers\getpass')->group(function() {
-    Route::get('/user', 'UserController@findAll');
+Route::controller(UserController::class)->group(function () {
+    Route::get('/usuario', 'findAll');
 });
